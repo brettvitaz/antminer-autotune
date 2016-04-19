83edb0e4b57b7869cd0cda975a85d044fe85a25b
Brett Vitaz <brett@vitaz.net>
2016-04-19 13:07:18 -0700

    use models from new models module.

--- antminer_autotune/antminer.py
+++ antminer_autotune/antminer.py
@@ -10,21 +10,7 @@ from paramiko import SSHClient, AutoAddPolicy
 from scp import SCPClient
 
 from antminer_autotune.util import makedir, fix_json_format
-
-models = {
-    's7': {
-        'ssh_port': 22,
-        'api_port': 4028,
-        'username': 'root',
-        'password': 'admin',
-        'min_freq': 100,
-        'max_freq': 700,
-        'min_temp': 72,
-        'max_temp': 76,
-        'dec_time': 30,
-        'inc_time': 900,
-    }
-}
+from antminer_autotune.models import models
 
 
 def ssh_client(fn):
@@ -65,12 +51,17 @@ class Antminer:
 
     def __init__(self, host, model, ssh_port=None, api_port=None, username=None, password=None):
         if isinstance(model, str):
-            model = models[model]
+            self.model = models[model].copy()
+        elif isinstance(model, dict):
+            self.model = model.copy()
+        else:
+            raise TypeError('Arg `model` must be a string (key from known models dict) or a dict.')
+
         self.host = host
-        self.ssh_port = int(ssh_port or model['ssh_port'])
-        self.api_port = int(api_port or model['api_port'])
-        self._username = username or model['username']
-        self._password = password or model['password']
+        self.ssh_port = int(ssh_port or self.model['ssh_port'])
+        self.api_port = int(api_port or self.model['api_port'])
+        self._username = username or self.model['username']
+        self._password = password or self.model['password']
 
         self._local_config_path = Path(host, self.CONFIG_FILE_NAME)
         self._remote_config_path = PosixPath(self.CONFIG_FILE_DIR, self.CONFIG_FILE_NAME)
@@ -96,8 +87,7 @@ class Antminer:
             raise ValueError('Frequency is not valid: {}'.format(value))
 
     def _is_valid_frequency(self, value):
-        return ((100 <= value <= 400 and value % 25 == 0) or
-                (400 <= value <= 700 and value % 6.25 == 0.0))
+        return value in (v.value for v in self.model['frequencies'])
 
     @property
     def fan_speed(self):
